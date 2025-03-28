a752447082496b8c94a4f722a393cfd9177e1873
Eliot Horowitz <eliothorowitz@gmail.com>
2025-03-28 17:09:06 -0400

    testing new idea

---
+++ flag.go
@@ -0,0 +1,36 @@
+package vmodutils
+
+import (
+	"context"
+	"flag"
+
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/robot/client"
+	"go.viam.com/utils/rpc"
+)
+
+type MachineSetup struct {
+	machine, apiKey, apiKeyId string
+}
+
+func (ms *MachineSetup) Connect(ctx context.Context, logger logging.Logger) (*client.RobotClient, error) {
+	return client.New(
+		ctx,
+		ms.machine,
+		logger,
+		client.WithDialOptions(rpc.WithEntityCredentials(
+			ms.apiKeyId,
+			rpc.Credentials{
+				Type:    rpc.CredentialsTypeAPIKey,
+				Payload: ms.apiKey,
+			})),
+	)
+}
+
+func AddMachineFlags() *MachineSetup {
+	ms := &MachineSetup{}
+	flag.StringVar(&ms.machine, "machine", "", "")
+	flag.StringVar(&ms.apiKey, "api-key", "", "")
+	flag.StringVar(&ms.apiKeyId, "api-key-id", "", "")
+	return ms
+}
