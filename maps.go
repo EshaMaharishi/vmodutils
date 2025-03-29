d4550590166669bd148cdcbf275c6e460c51fdbb
Eliot Horowitz <eliothorowitz@gmail.com>
2025-03-29 11:14:36 -0400

    helpers`

---
+++ maps.go
@@ -0,0 +1,17 @@
+package vmodutils
+
+func GetIntFromMap(m map[string]interface{}, n string) (int, bool) {
+	i, ok := m[n]
+	if !ok {
+		return 0, false
+	}
+
+	switch x := i.(type) {
+	case int:
+		return x, true
+	case float64:
+		return int(x), true
+	}
+
+	return 0, false
+}
---
+++ maps_test.go
@@ -0,0 +1,24 @@
+package vmodutils
+
+import (
+	"testing"
+
+	"go.viam.com/test"
+)
+
+func TestGetIntFromMap(t *testing.T) {
+	_, ok := GetIntFromMap(map[string]interface{}{}, "x")
+	test.That(t, ok, test.ShouldBeFalse)
+
+	_, ok = GetIntFromMap(map[string]interface{}{"x": "Asd"}, "x")
+	test.That(t, ok, test.ShouldBeFalse)
+
+	i, ok := GetIntFromMap(map[string]interface{}{"x": 5}, "x")
+	test.That(t, ok, test.ShouldBeTrue)
+	test.That(t, i, test.ShouldEqual, 5)
+
+	i, ok = GetIntFromMap(map[string]interface{}{"x": 5.1}, "x")
+	test.That(t, ok, test.ShouldBeTrue)
+	test.That(t, i, test.ShouldEqual, 5)
+
+}
