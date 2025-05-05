18ff6dc2d94c2fb27d8d5d895f1d550fef3c6dc3
Eliot Horowitz <eliothorowitz@gmail.com>
2025-05-05 00:25:52 -0400

    fix

--- touch/pcd.go
+++ touch/pcd.go
@@ -1,13 +1,21 @@
 package touch
 
-func PCDFindHighestInRegion(p pointcloud.PointCloud, box image.Rectangle) r3.Vector {
+import (
+	"image"
+
+	"github.com/golang/geo/r3"
+
+	"go.viam.com/rdk/pointcloud"
+)
+
+func PCDFindHighestInRegion(pc pointcloud.PointCloud, box image.Rectangle) r3.Vector {
 
 	best := r3.Vector{Z: -100000}
 
 	pc.Iterate(0, 0, func(p r3.Vector, d pointcloud.Data) bool {
 		if p.Z > best.Z {
-			if p.X >= box.Min.X && p.Y >= box.Min.Y {
-				if p.X <= box.Max.X && p.Y >= box.Max.Y {
+			if p.X >= float64(box.Min.X) && p.Y >= float64(box.Min.Y) {
+				if p.X <= float64(box.Max.X) && p.Y >= float64(box.Max.Y) {
 					best = p
 				}
 			}
