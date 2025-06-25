8e70a77bb6e46cd4b024be91e4413281f8efbb41
Eliot Horowitz <eliothorowitz@gmail.com>
2025-06-25 01:04:12 -0400

    add GetInt64FromMap

--- maps.go
+++ maps.go
@@ -13,8 +13,32 @@ func GetIntFromMap(m map[string]interface{}, n string) (int, bool) {
 	switch x := i.(type) {
 	case int:
 		return x, true
+	case int64:
+		return int(x), true
 	case float64:
 		return int(x), true
+	}
+
+	return 0, false
+}
+
+func GetInt64FromMap(m map[string]interface{}, n string) (int64, bool) {
+	if m == nil {
+		return 0, false
+	}
+
+	i, ok := m[n]
+	if !ok {
+		return 0, false
+	}
+
+	switch x := i.(type) {
+	case int64:
+		return x, true
+	case int:
+		return int64(x), true
+	case float64:
+		return int64(x), true
 	}
 
 	return 0, false
--- maps_test.go
+++ maps_test.go
@@ -22,3 +22,20 @@ func TestGetIntFromMap(t *testing.T) {
 	test.That(t, i, test.ShouldEqual, 5)
 
 }
+
+func TestGetInt64FromMap(t *testing.T) {
+	_, ok := GetInt64FromMap(map[string]interface{}{}, "x")
+	test.That(t, ok, test.ShouldBeFalse)
+
+	_, ok = GetInt64FromMap(map[string]interface{}{"x": "Asd"}, "x")
+	test.That(t, ok, test.ShouldBeFalse)
+
+	i, ok := GetInt64FromMap(map[string]interface{}{"x": 5}, "x")
+	test.That(t, ok, test.ShouldBeTrue)
+	test.That(t, i, test.ShouldEqual, 5)
+
+	i, ok = GetInt64FromMap(map[string]interface{}{"x": 5.1}, "x")
+	test.That(t, ok, test.ShouldBeTrue)
+	test.That(t, i, test.ShouldEqual, 5)
+
+}
