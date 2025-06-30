6d2c7825003594e295a45648c5eb6b8438f2768f
Eliot Horowitz <eliothorowitz@gmail.com>
2025-06-30 19:46:00 -0400

    more debugging tools

--- cmd/pctools/pctools-cmd.go
+++ cmd/pctools/pctools-cmd.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"image/png"
 	"os"
 
 	"go.viam.com/rdk/components/camera"
@@ -64,6 +65,64 @@ func realMain() error {
 		defer f.Close()
 
 		return pointcloud.ToPCD(pc, f, pointcloud.PCDBinary)
+	}
+
+	if *cmd == "realsense-all" {
+		machine, err := vmodutils.ConnectToHostFromCLIToken(ctx, *host, logger)
+		if err != nil {
+			return err
+		}
+		defer machine.Close(ctx)
+
+		myCamera, err := camera.FromRobot(machine, *cameraName)
+		if err != nil {
+			return err
+		}
+
+		pc, err := myCamera.NextPointCloud(ctx)
+		if err != nil {
+			return err
+		}
+
+		f, err := os.OpenFile("rs.pcd", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		err = pointcloud.ToPCD(pc, f, pointcloud.PCDBinary)
+		if err != nil {
+			return err
+		}
+
+		imgs, _, err := myCamera.Images(ctx)
+		if err != nil {
+			return err
+		}
+
+		for _, i := range imgs {
+			fn := fmt.Sprintf("rs-%s.png", i.SourceName)
+
+			f, err := os.OpenFile(fn, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			err = png.Encode(f, i.Image)
+			if err != nil {
+				return fmt.Errorf("cannot write (%s): %w", fn, err)
+			}
+		}
+
+		props, err := myCamera.Properties(ctx)
+		if err != nil {
+			return err
+		}
+		logger.Infof("props - IntrinsicParams %T %v", props.IntrinsicParams, props.IntrinsicParams)
+		logger.Infof("props - DistortionParams %T %v", props.DistortionParams, props.DistortionParams)
+
+		return nil
 	}
 
 	if *cmd == "size" {
